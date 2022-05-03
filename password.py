42089d6b1d57cfbc587a67e42075530bcbfe5243
黃光瑊 <genevieve1005ggsisteriscc@gmail.com>
2022-05-03 16:34:30 +0800

    change to while i>0

--- password.py
+++ password.py
@@ -1,1 +1,14 @@
-password.py
+password = 'g123456'
+i = 3
+while i > 0:
+    answer = input('請輸入密碼:')
+    if answer == password:
+        print('登入成功！')
+        break
+    else:
+        i = i - 1
+        print('密碼錯誤！還有', i, '次機會')
+        
+
+
+
