967816068fb7b8cdf757d90fa31aa7de47b0cfec
黃光瑊 <genevieve1005ggsisteriscc@gmail.com>
2022-05-03 16:43:34 +0800

    final version

--- password.py
+++ password.py
@@ -1,13 +1,17 @@
 password = 'g123456'
 i = 3
 while i > 0:
+    i = i - 1
     answer = input('請輸入密碼:')
     if answer == password:
         print('登入成功！')
         break
     else:
-        i = i - 1
-        print('密碼錯誤！還有', i, '次機會')
+        print('密碼錯誤！')
+        if i > 0 :
+            print('還有', i, '次機會')
+        else:
+            print('帳號已被鎖住')
         
 
 
